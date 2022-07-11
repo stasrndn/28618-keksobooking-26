b466261ee55a07a6cc457d1654f11a0c31163090
stasrndn <stas.rndn@yandex.ru>
2022-07-11 18:51:00 +0300

    Удаляет лишнее из main.js

--- js/main.js
+++ js/main.js
@@ -4,5 +4,3 @@ const SIMILAR_ADV_COUNT = 10;
 
 // eslint-disable-next-line no-unused-vars
 const similarAdvertisements = makeAdvertisements(SIMILAR_ADV_COUNT);
-
-console.log(similarAdvertisements);
