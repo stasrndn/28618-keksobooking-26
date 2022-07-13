196c03429acc1359036c98618444ecf70e37a36b
stasrndn <stas.rndn@yandex.ru>
2022-07-13 20:18:09 +0300

    Добавляет функционал переключения состояния страницы. Активное или неактивное

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 import {makeAdvertisements} from './data.js';
 import {render} from './template.js';
+import {disableActiveState} from './form.js';
+
 
 const SIMILAR_ADV_COUNT = 1;
 const similarAdvertisements = makeAdvertisements(SIMILAR_ADV_COUNT);
 
 render(similarAdvertisements);
+
+disableActiveState();
