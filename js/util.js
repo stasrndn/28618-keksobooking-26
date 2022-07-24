a94a5a2b5df98b6f556bc5525cb38b9f1777ddef
stasrndn <stas.rndn@yandex.ru>
2022-07-24 22:12:45 +0300

    Добавляет функции для избежания дребезга и прокупуска кадров

--- js/util.js
+++ js/util.js
@@ -13,7 +13,36 @@ const getDecOfNum = (number, titles) => {
   return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
 };
 
+/**
+ * Функция для устранения дребезга
+ */
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+/**
+ * Функция для пропуска кадров
+ */
+const throttle = (callback, delayBetweenFrames) => {
+  let lastTime = 0;
+
+  return (...rest) => {
+    const now = new Date();
+
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+};
+
 export {
   isEscapeKey,
   getDecOfNum,
+  debounce,
+  throttle,
 };
